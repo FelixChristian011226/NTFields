a1d5ac81dc781014a0244042cd9eb8b2a2cf132f
wangyuefei <1930552052@qq.com>
2024-04-24 23:47:32 +0800

    Add. Added comments on database.py.

--- models/database.py
+++ models/database.py
@@ -4,23 +4,27 @@ from torch import Tensor
 from torch.autograd import Variable, grad
 
 
-
+# 创建数据集类 `_numpy2dataset`，用于将numpy数组转换为PyTorch数据集
 class _numpy2dataset(torch.utils.data.Dataset):
     def __init__(self, points, speed, grid):
-        # Creating identical pairs
-        points    = Variable(Tensor(points))
+        # Creating identical pairs 将输入的点集和速度转换为PyTorch的`Tensor`类型，并使用`Variable`封装
+        points = Variable(Tensor(points))
         speed  = Variable(Tensor(speed))
+        # 将点集和速度连接起来形成数据
         self.data=torch.cat((points,speed),dim=1)
         self.grid  = Variable(Tensor(grid))
 
+    # 将数据和网格发送到指定的设备上
     def send_device(self,device):
         self.data    = self.data.to(device)
         self.grid    = self.grid.to(device)
 
-
+    # 获取指定索引的数据和索引值
     def __getitem__(self, index):
         data = self.data[index]
         return data, index
+
+    # 获取数据集的长度
     def __len__(self):
         return self.data.shape[0]
 
