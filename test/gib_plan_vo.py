fa2632ae8963f9f8e3a1718c0be67b389eab2f45
FelixChristian <1930552052@qq.com>
2024-05-13 04:34:01 +0800

    Add. Added collision avoidance.

--- test/gib_plan_vo.py
+++ test/gib_plan_vo.py
@@ -69,6 +69,7 @@ colors = [
 # 创建点云对象列表
 point_clouds = []
 index = 0
+safe_dis = 0.06
 
 # 初始化，存入起点终点，not_done数组记录机器人是否已经规划完毕，point[]记录路径点
 XP = torch.zeros((robot_num, 6))  # 初始化一个形状为 (robot_num, 6) 的零张量
@@ -84,8 +85,10 @@ iter = 0
 while any(not_done):
     print("iter",iter)
 
-    # 遍历每个机器人
+    # 遍历每个机器人，获得速度
     for i in range(robot_num):
+        if(not_done[i]==False):
+            continue
 
         # 将 XP[i] 变形为 (1, 6) 的形状
         XP_i = XP[i].view(1, 6).to('cuda')
@@ -104,6 +107,43 @@ while any(not_done):
 
         # 更新 XP[i]
         XP[i] = XP_i.view(-1).to('cpu')  # 将 XP_i 变回一维张量并移回 CPU
+
+    # 第二次遍历，计算机器人间的碰撞，碰撞则进行调整
+    for i in range(robot_num):
+        if(not_done[i]==False):
+            continue
+
+        for j in range(i+1, robot_num):
+            if(not_done[j]==False):
+                continue
+
+            XP1 = XP[i].view(1, 6).to('cuda')
+            XP2 = XP[j].view(1, 6).to('cuda')
+
+            rbt_dis = torch.norm(XP1[:, 0:3] - XP2[:, 0:3])
+            if rbt_dis < safe_dis:
+                print("collision between robot", i, "and robot", j)
+                collision += 1
+
+                # 计算调整量
+                # adjust = 0.03 * (XP1[:, 0:3] - XP2[:, 0:3]) / dis
+                adjust = (safe_dis-rbt_dis) *0.5 * (XP1[:, 0:3] - XP2[:, 0:3])
+
+                XP1[:, 0:3] += adjust
+                XP2[:, 0:3] -= adjust
+
+                # 回退到上一步
+                # point[i].pop()
+                # point[j].pop()
+                del point[i][-1]
+                del point[j][-1]
+                print("poped!")
+
+                # 将调整后的路径点添加到列表中
+                point[i].append(XP1[:, 0:3])
+                point[j].append(XP2[:, 0:3])
+                XP[i] = XP1.view(-1).to('cpu')
+                XP[j] = XP2.view(-1).to('cpu')
 
 
     iter=iter+1
